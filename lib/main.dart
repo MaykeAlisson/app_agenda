cb0381d086b3254cd4254bdee9db6416058d3b8a
Mayke Alisson <maykealison@gmail.com>
2019-11-11 06:18:55 -0300

    Editando um contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:agenda_app/ui/home_page.dart';
+import 'package:agenda_app/ui/contact_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false, // Remove o icone de degub
   ));
 }
