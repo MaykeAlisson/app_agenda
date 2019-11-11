6b246a76cfac59e17cec2fde6870d376a02f01b8
Mayke Alisson <maykealison@gmail.com>
2019-11-11 05:35:00 -0300

    Criando card para contatos

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -99,6 +99,8 @@ class Contact {
   String phone;
   String img;
 
+  Contact();
+
   Contact.fromMap(Map map) {
     id = map[idColunm];
     name = map[nameColunm];
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:agenda_app/ui/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-
+    home: HomePage(),
   ));
 }
